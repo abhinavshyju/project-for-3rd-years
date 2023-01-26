3bcd12dc4dbd47d4fa961c4466c450bfb158e7a6
Abhinav shyju <96369574+abhinavshyju@users.noreply.github.com>
2023-01-27 00:44:47 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+num = input("enter the number : ")
+sum = 0
+for i in (num):
+    i = int(i)
+    sum = sum + i
+# print(sum)
+# print(len(num))
+length = len(num)
+average = sum / length
+print("the average value is" ,average)
